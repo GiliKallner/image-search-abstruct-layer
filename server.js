454eb7b82e58392620909e332a51389902c7535b
Glitch (imagesrc) <none>
2017-10-31 23:13:47 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/routes.js');
 const mongoose = require('mongoose');
-const mongo = require('mongodb').MongoClient;
+//const mongo = require('mongodb').MongoClient;
 
-mongo.connect(process.env.MONGOLAB_URI,(err,data)=>{
-  if(err) throw err;
-  else console.log(data);
-});
+//mongo.connect(process.env.MONGOLAB_URI,(err,data)=>{
+  //if(err) throw err;
+  //else console.log(data);
+//});
 //mongo.Promise = global.Promise;
 
 app.use('/', routes);
