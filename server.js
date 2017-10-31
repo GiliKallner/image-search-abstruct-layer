a5d2b04566c4ea7585aaef67796a19e6a7d91e27
Glitch (imagesrc) <none>
2017-10-31 21:06:35 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes.js');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.Promise = global.Promise;
 
 app.use('/', routes);
+
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function () {
