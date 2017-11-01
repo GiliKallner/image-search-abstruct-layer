16e6cd5dd1de2e1aebb78203dc167c20387cae85
Glitch (imagesrc) <none>
2017-11-01 08:04:25 +0000

    Checkpoint

--- db-models/history.js
+++ db-models/history.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
-    name: String,
-    when: {type:Date,default:Date.now()}
+    name:String,
+    date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('History',schema); 
--- routes/routes.js
+++ routes/routes.js
@@ -2,18 +2,22 @@ const express = require('express');
 const router = express.Router();
 const imgur = require('../services/imgur.js');
 const path = require('path');
-
+const history = require('../db-models/history.js');
 //const views = require('../views/index.html');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => {  
   res.sendFile(path.join('/app/views/index.html'));
 });
 
 router.get('/latest', (req, res) => {
+  
 });
 
-router.get('/search/:q', (req, res) => {
-  imgur.create_image(req.params.q, req.query.offset).then(ans => {
+router.get('/search/:query', (req, res) => {
+  imgur.create_image(req.params.query, req.query.offset).then(ans => {
+    
+    new history({ name:req.params.query, date: new Date().now() }).save();
+    
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(ans,null,3));
   })
